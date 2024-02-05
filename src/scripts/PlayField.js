c7dd152ca32b25fcd59f2e8b8e92de0e0a9030fa
Alexandra Paramonova <alpar.wb@gmail.com>
2024-02-05 17:18:29 +0500

    feat: implement PlayField class

---
+++ src/scripts/PlayField.js
@@ -0,0 +1,74 @@
+import { constructBoardCells } from "./play-field/constructBoardCells";
+import { countSumCellsForPlayField } from "./play-field/countSumCellsForPlayField";
+import { countFilledCellsInPuzzle } from "./play-field/countFilledCellsInPuzzle";
+import { initializeBoardStateMatrix } from "./play-field/initializeBoardStateMatrix";
+import { isEmptyBoardStateMatrix } from "./play-field/isEmptyBoardStateMatrix";
+
+export class PlayField {
+  #gameBoard;
+
+  #puzzleMatrix;
+
+  #numFilledCellsPuzzle;
+
+  #sumCellsPlayField;
+
+  #boardStateMatrix;
+
+  #boardCells;
+
+  constructor(gameBoard, puzzleMatrix, boardStateMatrix) {
+    this.#gameBoard = gameBoard;
+    this.#puzzleMatrix = puzzleMatrix;
+    this.#numFilledCellsPuzzle = countFilledCellsInPuzzle(puzzleMatrix);
+    if (isEmptyBoardStateMatrix(boardStateMatrix)) {
+      this.#boardStateMatrix = initializeBoardStateMatrix(puzzleMatrix);
+      this.#sumCellsPlayField = -1 * this.#numFilledCellsPuzzle;
+    } else {
+      this.#boardStateMatrix = boardStateMatrix;
+      this.#sumCellsPlayField = countSumCellsForPlayField(
+        this.#boardStateMatrix,
+        this.#puzzleMatrix
+      );
+    }
+    this.#constructBoardCells();
+  }
+
+  #constructBoardCells() {
+    // TODO: check that size of puzzleMatrix and boardStateMatrix is the same
+    this.#boardCells = constructBoardCells(
+      this.#gameBoard,
+      this.#puzzleMatrix,
+      this.#boardStateMatrix
+    );
+  }
+
+  #handleWinGame() {
+    // TODO: show message
+    console.log("Win!", this.#numFilledCellsPuzzle, this.#sumCellsPlayField);
+  }
+
+  handleAction(
+    cellIndexRow,
+    cellIndexColumn,
+    actionType,
+    canvasContext,
+    boardSettings,
+    board
+  ) {
+    const iRow = cellIndexRow;
+    const jCol = cellIndexColumn;
+    const cell = this.#boardCells[iRow][jCol];
+    const { newCellState, stateDelta } = cell.handleAction(
+      actionType,
+      canvasContext,
+      boardSettings,
+      board
+    );
+    this.#boardStateMatrix[iRow][jCol] = newCellState;
+    this.#sumCellsPlayField += stateDelta;
+    if (this.#sumCellsPlayField === this.#numFilledCellsPuzzle) {
+      this.#handleWinGame();
+    }
+  }
+}
