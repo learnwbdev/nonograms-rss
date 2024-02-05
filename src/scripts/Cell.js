ae69ab6194dd7a0f3efdfa1101c1d80c1bf70bb2
Alexandra Paramonova <alpar.wb@gmail.com>
2024-02-05 17:14:55 +0500

    feat: implement Cell class

---
+++ src/scripts/Cell.js
@@ -0,0 +1,69 @@
+import { actionTypes } from "./actions/actionTypes";
+import { changeCellContent } from "./cell/changeCellContent";
+import {
+  getStateAfterMainAction,
+  getStateAfterSubAction,
+} from "./cell/changeState";
+import { getDeltaForNewState } from "./cell/getDeltaForNewState";
+
+export class Cell {
+  #xLeft;
+
+  #yTop;
+
+  #state;
+
+  #correctVal;
+
+  #actions = actionTypes;
+
+  constructor(xLeft, yTop, currentState, correctValue) {
+    this.#xLeft = xLeft;
+    this.#yTop = yTop;
+    this.#state = currentState;
+    this.#correctVal = correctValue;
+  }
+
+  #changeState(newState, canvasContext, boardSettings, board) {
+    const prevState = this.#state;
+    this.#state = newState;
+    changeCellContent(canvasContext, this, boardSettings, board);
+    const stateDelta = getDeltaForNewState(
+      this.#state,
+      this.#correctVal,
+      prevState
+    );
+    return stateDelta;
+  }
+
+  getCellData() {
+    return {
+      x: this.#xLeft,
+      y: this.#yTop,
+      state: this.#state,
+    };
+  }
+
+  handleAction(actionType, canvasContext, boardSettings, board) {
+    const actions = this.#actions;
+    let newState;
+    switch (actionType) {
+      case actions.mainAction:
+        newState = getStateAfterMainAction(this.#state);
+        break;
+      case actions.subAction:
+        newState = getStateAfterSubAction(this.#state);
+        break;
+      default:
+        break;
+    }
+    const stateDelta = this.#changeState(
+      newState,
+      canvasContext,
+      boardSettings,
+      board
+    );
+    // TODO: play sound effect
+    return { newState, stateDelta };
+  }
+}
