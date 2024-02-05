ea7823dc3dc992f3c852d940b6f80a53c7b3cb9a
Alexandra Paramonova <alpar.wb@gmail.com>
2024-02-05 17:28:56 +0500

    feat: implement Game class

---
+++ src/scripts/Game.js
@@ -0,0 +1,33 @@
+import { decodePuzzle } from "./game/decodePuzzle";
+import { formPuzzleClues } from "./game/formPuzzleClues";
+import { GameBoard } from "./GameBoard";
+
+export class Game {
+  #nonogram = {};
+
+  #gameBoard;
+
+  #isGameInProgress = false;
+
+  #gameDurationInSec = 0;
+
+  constructor(canvasClassName) {
+    this.#gameBoard = new GameBoard(canvasClassName);
+  }
+
+  setNonogram(nonogram) {
+    this.#nonogram = nonogram;
+    const puzzleMatrix = decodePuzzle(this.#nonogram.puzzle);
+    const { rowsAllClues, colsAllClues } = formPuzzleClues(puzzleMatrix);
+    const boardStateMatrix = [];
+    this.#gameBoard.drawBoard(
+      puzzleMatrix,
+      { rowsAllClues, colsAllClues },
+      boardStateMatrix
+    );
+  }
+
+  showSolution() {
+    this.#gameBoard.drawSolution();
+  }
+}
