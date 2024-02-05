a2c6cde234f0744635340ffbfe996d7e45f5c6d2
Alexandra Paramonova <alpar.wb@gmail.com>
2024-02-06 02:57:51 +0500

    feat: add createRandomButton function to create section with random button

---
+++ src/scripts/layout/createRandomButton.js
@@ -0,0 +1,10 @@
+export const createRandomButton = () => {
+  const randomSection = document.createElement("section");
+  randomSection.classList.add("random");
+  const randomBtn = document.createElement("button");
+  randomBtn.classList.add("random__btn");
+  randomBtn.innerText = "Random game";
+  randomSection.appendChild(randomBtn);
+  document.body.appendChild(randomSection);
+  return randomBtn;
+};
