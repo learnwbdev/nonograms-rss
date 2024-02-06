d84bb56fdbf1ce1d3ef1f68bb5f1615d2606c981
Alexandra Paramonova <alpar.wb@gmail.com>
2024-02-06 09:39:45 +0500

    fix: add input parameter for toggleTheme function

--- src/scripts/theme/toggleTheme.js
+++ src/scripts/theme/toggleTheme.js
@@ -1,9 +1,7 @@
 import { changeTheme } from "./changeTheme";
-import { getThemeName } from "./getThemeName";
 import { themes } from "./themesList";
 
-export const toggleTheme = () => {
-  const curThemeName = getThemeName();
+export const toggleTheme = (curThemeName) => {
   const newThemeName = themes.filter((theme) => theme !== curThemeName)[0];
   changeTheme(newThemeName);
 
