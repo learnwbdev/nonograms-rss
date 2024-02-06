33399bca9228f6cddd700ee6eb4c8d7bbd2b3bb2
Alexandra Paramonova <alpar.wb@gmail.com>
2024-02-06 09:17:06 +0500

    fix: delete console messages for getLatestWins

--- src/scripts/latest-wins/getLatestWins.js
+++ src/scripts/latest-wins/getLatestWins.js
@@ -1,6 +1,5 @@
 export const getLatestWins = () => {
   const latestWinsDefaultArr = "[]";
-  console.log(localStorage.latestWins);
   const latestWinsArr = JSON.parse(
     localStorage.latestWins ?? latestWinsDefaultArr
   );
