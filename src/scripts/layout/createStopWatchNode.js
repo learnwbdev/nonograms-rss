785390bdca184b2e5e007d96ff3a4fdc14bdc412
Alexandra Paramonova <alpar.wb@gmail.com>
2024-02-06 19:51:44 +0500

    refactor: delete replaced frunction createStopWatchNode, changed to createStatusSection function

--- src/scripts/layout/createStopWatchNode.js
+++
@@ -1,6 +0,0 @@
-export const createStopWatchNode = (className) => {
-  const stopWatchNode = document.createElement("div");
-  stopWatchNode.classList.add(className);
-  document.body.append(stopWatchNode);
-  return stopWatchNode;
-};
